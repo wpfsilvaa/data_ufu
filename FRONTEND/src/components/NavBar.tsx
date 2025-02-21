bf9656f7bf72c3ac46090a5c4f65fd7410c57ccb
wpfsilvaa <wpf.silvaa@outlook.com>
2025-02-21 10:09:04 -0300

    atualização design navbar

--- FRONTEND/src/components/NavBar.tsx
+++ FRONTEND/src/components/NavBar.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NavBar: React.FC = () => {
   return (
-    <nav className='p-2 mg-4'>
-      <ul className="flex justify-center space-x-8">
+    <nav className=''>
+      <ul className="flex flex-col md:flex-row md:space-x-2 items-center justify-center">
       <li className='transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-lg hover:shadow-blue-500/50 hover:bg-sky-600 bg-blend-screen p-3'><a href="\" className="">Inicio</a></li>
       <li className='transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-lg hover:shadow-blue-500/50 hover:bg-sky-600 bg-blend-screen p-3'><a href="#" className="">Sobre</a></li>
       <li className='transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-lg hover:shadow-blue-500/50 hover:bg-sky-600 bg-blend-screen p-3'><a href="#" className="">Área do Aluno</a></li>
